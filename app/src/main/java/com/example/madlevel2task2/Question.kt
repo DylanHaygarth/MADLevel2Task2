4156e823892a783413c1ef1111d7e60cda39416c
dylan2050 <dyhayg@gmail.com>
2020-09-13 11:59:02 +0200

    created questions adapter

---
+++ app/src/main/java/com/example/madlevel2task2/Question.kt
@@ -0,0 +1,15 @@
+package com.example.madlevel2task2
+
+data class Question (
+    var question : String,
+    var answer : Boolean
+) {
+    companion object {
+        val QUESTIONS = arrayOf(
+            "A 'val' and 'var' are the same.",
+            "Mobile Application Development grants 12 ECTS.",
+            "A unit in Kotlin corresponds to a void in Java.",
+            "In Kotlin 'when' replaces the 'switch' operator in Java."
+        )
+    }
+}
